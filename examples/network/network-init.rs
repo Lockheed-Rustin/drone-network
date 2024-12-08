554fc6fc78ade861c9e6f06ac103fc74a2e67f3d
DiCesareDaniele <dicesaredaniele7@gmail.com>
2024-12-08 06:23:11 +0100

    test

--- examples/network/network-init.rs
+++ examples/network/network-init.rs
@@ -5,6 +5,7 @@ fn main() {
     let file_str = fs::read_to_string("config.toml").unwrap();
     let config = toml::from_str(&file_str).unwrap();
     let controller = network::init_network(&config).unwrap();
+    println!("test");
     println!("{:#?}", controller);
     // let the threads live
     loop {}
