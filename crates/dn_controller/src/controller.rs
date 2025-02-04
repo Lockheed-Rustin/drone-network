2806a34cf77e7652d368f9a60ce4803931a3a2e9
Lorenzo Ferranti <lorenzoferranti04@outlook.it>
2025-02-04 09:25:37 +0100

    re-added shortcut function

--- crates/dn_controller/src/controller.rs
+++ crates/dn_controller/src/controller.rs
@@ -212,6 +212,12 @@ impl SimulationController {
         sender.send(ClientCommand::SendMessage(body, dest)).ok()
     }
 
+    pub fn shortcut(&self, p: Packet) -> Option<()> {
+        let dest_id = p.routing_header.hops.last()?;
+        let sender = &self.nodes.get(&dest_id)?.packet_send;
+        sender.send(p).ok()
+    }
+
     // TODO: remove this after the fair
     pub fn send_fragment_fair(&self, id: NodeId) -> Option<()> {
         let sender = self.get_client_sender(id)?;
