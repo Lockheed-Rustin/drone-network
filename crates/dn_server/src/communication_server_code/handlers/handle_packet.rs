8028edb3501de2699d1ed172e2db629853cd4849
Luca Agostinelli <luca@agostinelli.net>
2025-02-05 15:13:27 +0100

    handle packet fix

--- crates/dn_server/src/communication_server_code/handlers/handle_packet.rs
+++ crates/dn_server/src/communication_server_code/handlers/handle_packet.rs
@@ -29,33 +29,37 @@ impl CommunicationServer {
             .send(ServerEvent::PacketReceived(packet.clone(), self.id))
             .expect("Error in controller_send");
 
-        if self.check_routing(&packet) {
-            let sender_id = packet.routing_header.hops[0];
-            match packet.pack_type {
-                PacketType::MsgFragment(f) => self.handle_fragment(
-                    f,
-                    sender_id,
-                    packet.session_id,
-                    packet.routing_header.hops,
-                ),
-                PacketType::Nack(nack) => {
-                    self.handle_nack(nack, packet.session_id, packet.routing_header)
-                }
-                PacketType::Ack(ack) => {
-                    // update the estimated pdr for the last path used to go to the ack sender
-                    for n in self
-                        .network_topology
-                        .get_saved_path(&sender_id)
-                        .iter()
-                        .skip(1)
-                    {
-                        self.network_topology.update_estimated_pdr(*n, false);
-                    }
-                    self.session_manager.handle_ack(ack, &packet.session_id);
-                }
-                PacketType::FloodRequest(f_req) => self.send_flood_response(f_req),
-                PacketType::FloodResponse(f_res) => self.handle_flood_response(f_res),
+        if let PacketType::FloodRequest(f_req) = packet.pack_type {
+            self.send_flood_response(f_req);
+            return;
+        }
+
+        if !self.check_routing(&packet) {
+            return;
+        }
+
+        let sender_id = packet.routing_header.hops[0];
+        match packet.pack_type {
+            PacketType::MsgFragment(f) => {
+                self.handle_fragment(f, sender_id, packet.session_id, packet.routing_header.hops)
             }
+            PacketType::Nack(nack) => {
+                self.handle_nack(nack, packet.session_id, packet.routing_header)
+            }
+            PacketType::Ack(ack) => {
+                // update the estimated pdr for the last path used to go to the ack sender
+                for n in self
+                    .network_topology
+                    .get_saved_path(&sender_id)
+                    .iter()
+                    .skip(1)
+                {
+                    self.network_topology.update_estimated_pdr(*n, false);
+                }
+                self.session_manager.handle_ack(ack, &packet.session_id);
+            }
+            PacketType::FloodResponse(f_res) => self.handle_flood_response(f_res),
+            _ => {}
         }
     }
 
