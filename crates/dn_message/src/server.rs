36ef23028d1e2d05889f7d57e35ab023389019a7
Lorenzo Ferranti <lorenzoferranti04@outlook.it>
2025-02-10 10:58:24 +0100

    ignored clippy module_name_repetition in dn_message

--- crates/dn_message/src/server.rs
+++ crates/dn_message/src/server.rs
@@ -2,6 +2,7 @@ use super::CommunicationMessage;
 use bincode::{Decode, Encode};
 use wg_2024::network::NodeId;
 
+#[allow(clippy::module_name_repetitions)]
 #[derive(Debug, Clone, Encode, Decode)]
 pub enum ServerBody {
     RespServerType(ServerType),
@@ -10,12 +11,14 @@ pub enum ServerBody {
     ServerCommunication(ServerCommunicationBody),
 }
 
+#[allow(clippy::module_name_repetitions)]
 #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
 pub enum ServerType {
     Content,
     Communication,
 }
 
+#[allow(clippy::module_name_repetitions)]
 #[derive(Debug, Clone, Encode, Decode)]
 pub enum ServerContentBody {
     RespFilesList(Vec<String>),
@@ -29,6 +32,7 @@ pub enum FileType {
     Text,
 }
 
+#[allow(clippy::module_name_repetitions)]
 #[derive(Debug, Clone, Encode, Decode)]
 pub enum ServerCommunicationBody {
     RespClientList(Vec<NodeId>),
